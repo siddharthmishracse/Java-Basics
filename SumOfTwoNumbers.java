4aa5d81322d0faa6b850f478e438648b916b91f4
siddharthmishrase26 <siddharthmishrase26@gmail.com>
2017-03-11 14:41:17 +0530

    Sum of Two Numbers

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,15 @@
+class SumOfTwoNumbers{
+
+	public static void main(String [] args){
+
+		int a;		//Initialization
+		int b;		//Initialization
+		int sum;                    
+		a=Integer.parseInt(args[0]);   //Inputted Through Command Line	
+		b=Integer.parseInt(args[1]);   //Inputted Through Command Line
+
+	
+		sum=a+b;	
+		System.out.print("Sum of Two Numbers= "+sum);  //Display The sum 
+	}
+}
